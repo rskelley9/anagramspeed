f033190068968313f3631cbf2af8e1d785547ac0
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 18:46:01 -0500

    cleaned up formatting of the model

--- app/models/word.rb
+++ app/models/word.rb
@@ -7,26 +7,16 @@ class Word < ActiveRecord::Base
   def self.anagrams(input)
     array = []
     Word.all.each do |dict_word|
-     if Word.canonical(dict_word.word) == Word.canonical(input)
-      array << dict_word
+      if Word.canonical(dict_word.word) == Word.canonical(input)
+        array << dict_word
+      end
+    end
+    array
+  end
+
+  def self.anagram(input)
+    Word.all.select do |dict_word|
+      canonical(dict_word.word) == canonical(input)
     end
   end
-  array
-end
-
-def self.anagram(input)
-  Word.all.select do |dict_word|
-    canonical(dict_word.word) == canonical(input)
-  end
-end
-
-
-def self.match?(word)
-  Word.where(:word => word)
-end
-
-  # def self.anagrams(word)
-  #   word
-  # end
-
 end
