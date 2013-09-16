c4ad6cc7302eeea2b199de12857394c2c956740e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 18:47:14 -0500

    fixed formatting of seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 
 
 File.open("dictionary.txt","r").each_line do |word|
-  # while(line = word.gets)
-    Word.create(word: word.chomp!, canonical: word.downcase.split('').sort.join)
-  # end
+  Word.create(word: word.chomp!, canonical: word.downcase.split('').sort.join)
 end
 
 
